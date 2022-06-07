a26fc80d47b1a6e16b343ca065eaebafa3fbc6a1
szakakos <szakallasakos@gmail.com>
2022-06-07 17:49:12 -0400

    Added the modified sysinfo file.

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-echo "Host Name: $(hostname)" #Display the hostname variable with formatting
-echo "DNS Domain Name: "`hostname -f` #Display the DNS Domain
-echo `hostnamectl | grep "Operating System"` #Get just the operating system value from hostnamectl
-echo  "IP Addressing Information: \\n" `ifconfig | grep inet | awk -F ' ' '{print $2}' | grep -v '^172'`
-#Invoke ifconfig, only take lines with inet, seperate the data and print it, only display ips beginning in 172.
-echo "Root Filesystem Status: "
-df -hT /
-#Invoke DF with human readable formatting, reports the system type, and only displays the main drive.
+OS=`hostnamectl | grep "Operating System"` #Get OS name useing hostnamectl and filtering the operating system line.
+IP=`hostname -I` #Get the IP with hostnamectl, with the I argument.
+Space=`df -hT / | awk '{print $4}' | tr -d '\n' | cut -c 5-` #Get the space, get the fourth data set, remove new lines, and then only get after the first 5 characters
+echo -e "\n" #new line
+echo "Report for $(hostname)" #Introductory message
+echo "===============" #Divider
+echo "FQDN: "`hostname -f` #Formatted output for fully qualified domain name
+echo "Operating System name and version: $OS" #Formatted output for OS
+echo  "IP Address: $IP" #Formatted output of IP addess
+echo "Root Filesystem Free Space: $Space" #Formatted output of space left
+echo "===============" #Divider
+echo -e "\n" #new line
